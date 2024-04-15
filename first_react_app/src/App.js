dacd6339319b821f2fd9c361d1cdd2721292ee0d
Mustafa <mssheliyait@gmail.com>
2024-04-15 15:49:09 +0530

    add first react & vite + react project

---
+++ first_react_app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+      <h1>Wllcome to first React App | Mustafa</h1>
+  );
+}
+
+export default App;
---
+++ first_vite_react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+      <h1>Wellcome To First Vite + React App | Mustafa Aslam</h1>
+  )
+}
+
+export default App
